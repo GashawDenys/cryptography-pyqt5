ec2b5b3f09760691f85da5b94ddfe179ab6cc1ae
denysgashaw <denys.gashaw@gmail.com>
2019-11-17 01:21:42 +0200

    Completed encryption

--- MainWindow.py
+++ MainWindow.py
@@ -8,6 +8,12 @@
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+import random
+import textwrap
+
+size = random.randint(3, 5)
+n = random.randint(50, 53)
+m = random.randint(300, 400)
 
 
 class Ui_MainWindow(object):
@@ -15,20 +21,20 @@ class Ui_MainWindow(object):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(585, 494)
         MainWindow.setStyleSheet("*{\n"
-"background-image: url(/Users/denysgashaw/Desktop/КПИ/3 курс/6labcipher/img/cryptoimg.png);\n"
-"}\n"
-"QPushButton:hover{\n"
-"    color: black;\n"
-"}\n"
-"")
+                                 "background-image: url(/Users/denysgashaw/Desktop/КПИ/3 курс/6labcipher/img/cryptoimg.png);\n"
+                                 "}\n"
+                                 "QPushButton:hover{\n"
+                                 "    color: black;\n"
+                                 "}\n"
+                                 "")
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
         self.textEdit.setGeometry(QtCore.QRect(110, 40, 371, 401))
         self.textEdit.setStyleSheet("#textEdit{\n"
-"    background: grey;\n"
-"}\n"
-"")
+                                    "    background: grey;\n"
+                                    "}\n"
+                                    "")
         self.textEdit.setObjectName("textEdit")
         self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
         self.lineEdit.setGeometry(QtCore.QRect(160, 120, 281, 31))
@@ -41,27 +47,28 @@ class Ui_MainWindow(object):
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton.setGeometry(QtCore.QRect(150, 310, 121, 41))
         self.pushButton.setStyleSheet("color: white;\n"
-"border-radius: 20px;\n"
-"\n"
-"")
+                                      "border-radius: 20px;\n"
+                                      "\n"
+                                      "")
         self.pushButton.setObjectName("pushButton")
         self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_2.setGeometry(QtCore.QRect(330, 310, 121, 41))
         self.pushButton_2.setStyleSheet("color: white;\n"
-"border-radius: 20px;")
+                                        "border-radius: 20px;")
         self.pushButton_2.setObjectName("pushButton_2")
         self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_3.setGeometry(QtCore.QRect(220, 370, 161, 51))
         self.pushButton_3.setStyleSheet("\n"
-"color: white;\n"
-"border-radius: 20px")
+                                        "color: white;\n"
+                                        "border-radius: 20px")
         self.pushButton_3.setObjectName("pushButton_3")
         self.toolButton = QtWidgets.QToolButton(self.centralwidget)
         self.toolButton.setGeometry(QtCore.QRect(260, 20, 71, 51))
         self.toolButton.setStyleSheet("border-radius: 20px;")
         self.toolButton.setText("")
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap("img/key-emoji1.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon.addPixmap(QtGui.QPixmap("img/key-emoji1.png"),
+                       QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.toolButton.setIcon(icon)
         self.toolButton.setObjectName("toolButton")
         self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
@@ -76,6 +83,11 @@ class Ui_MainWindow(object):
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
+        #logic
+        
+        self.pushButton_3.clicked.connect(lambda: self.public_key(n, m, size))
+        self.pushButton.clicked.connect(self.encryption)
+        
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
@@ -83,12 +95,75 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.lineEdit.setPlaceholderText(_translate("MainWindow", "Введите начальный текст"))
-        self.lineEdit_3.setPlaceholderText(_translate("MainWindow", "Открытый ключ"))
+        self.lineEdit.setPlaceholderText(_translate(
+            "MainWindow", "Введите начальный текст"))
+        self.lineEdit_3.setPlaceholderText(
+            _translate("MainWindow", "Открытый ключ"))
         self.pushButton.setText(_translate("MainWindow", "Зашифровать"))
         self.pushButton_2.setText(_translate("MainWindow", "Разшифровать"))
-        self.pushButton_3.setText(_translate("MainWindow", "Сгенерировать ключи"))
-        self.lineEdit_2.setPlaceholderText(_translate("MainWindow", "Результат"))
+        self.pushButton_3.setText(_translate(
+            "MainWindow", "Сгенерировать ключи"))
+        self.lineEdit_2.setPlaceholderText(
+            _translate("MainWindow", "Результат"))
+
+    def generate_super_increasing_knapsack(self, size):
+        first = random.randint(2, 2+size)
+        knapsack = [first]
+        sum = first
+        for _ in range(size-1):
+            element = sum + random.randint(1, size)
+            knapsack.append(element)
+            sum += element
+        print("Это ваш приватный ключ:", knapsack)
+        return knapsack
+
+    def public_key(self, n, m, size):
+        private_key = self.generate_super_increasing_knapsack(size)
+        suma = sum(private_key)
+        if suma >= m:
+            raise AttributeError(
+                'm должно быть больше чем сумма всех элементов приватного ключа')
+        public_key_int = [(k*n) % m for k in private_key]
+        public_key_string = " ".join(map(str,public_key_int)) #конвертируем лист интов в строку
+        print("N = {} и M = {}".format(n,m))
+        self.lineEdit_3.setText(public_key_string)
+
+
+    def plain_text_to_binary(self):
+        plain_text = self.lineEdit.text()
+        binary_str = ''.join(format(ord(x), 'b') for x in plain_text)
+        binary_str = textwrap.wrap(binary_str, size)
+        last_element = list(binary_str.pop())
+        while len(last_element) < size:
+              last_element.insert(0, '0')
+        new_string = ''.join(last_element)
+        binary_str.append(new_string)
+        print(binary_str)
+        return binary_str
+       
+        
+    def type_convertation(self, public_key):
+        splited_string = public_key.split(" ")
+        splited_int_list = [int(d) for d in splited_string]
+        return splited_int_list
+
+    def encryption(self):
+        public_key = self.lineEdit_3.text()
+        public_key = self.type_convertation(public_key)
+        binary_str = self.plain_text_to_binary()
+        binary_list = [list(d) for d in binary_str]
+        number_list = [[int(y) for y in x] for x in binary_list]
+        print(binary_list)
+        result = []
+        for i in range(len(number_list)):
+            result.append(0)
+
+        for i in range(len(number_list)):
+            for j in range(len(public_key)):
+                result[i] += public_key[j] * number_list[i][j]
+        result = str(result)
+        self.lineEdit_2.setText(result)
+     
 
 if __name__ == "__main__":
     import sys
